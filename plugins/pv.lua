9c5d92edfff1660e3d4f96408024b0d2ac4b647f
haker20154 <behrang.parsa94@gmail.com>
2016-10-07 08:20:05 +0330

    Update pv.lua

--- plugins/pv.lua
+++ plugins/pv.lua
@@ -2,7 +2,7 @@ function run(msg, matches)
 if msg.to.type == 'user' and not is_sudo(msg) then
 return [[
 اینجا پیوی رباته لطفا اگه کاری دارید پیام خودتونو ارسال کنید تا مدیران ربات به شما جواب بدهند.
-با تشکر SurenaTeam
+با تشکر BlasterTeam
 ]]
 end
 end
